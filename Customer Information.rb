7cdbe2169fe48b0d4cf799447f0642fb0cce531f
WoleCTO <102435582+WoleCTO@users.noreply.github.com>
2025-04-01 19:20:07 -0400

    Adding fixed Render Pipeline loading and also a hint to controlling the avatar from the new input system

---
+++ Customer Information.rb
@@ -0,0 +1,30 @@
+Customer Information 
+Company name: Toyz Electronics
+Company website: 
+Please provide a brief description of your business: 
+
+Email Service Information 
+Subscription ID: 
+Azure Communication Services Resource Name: DahVarsityEmail
+Is your custom domain already set up and currently used for sending messages:  
+Indicate the domain from which you are currently sending emails:  
+
+Usage Information
+1. What type of emails do you send? (such as Transactional, Marketing, Promotional) 
+2. Please specify the expected volume of emails you plan to send:  
+	- What is the maximum rate of messages per minute that you require? 1000
+	- What is the maximum rate of messages per hour that you require? 1000
+	- What is the maximum rate of messages per day that you require? 10000
+
+Additional Information 
+What is the source of the email addresses that you use for sending your messages? dahvarsityai.com
+Note: The source of the email addresses that you send your messages to plays a crucial role in the 
+effectiveness and compliance of your email marketing campaigns. Providing details about the source 
+of your email addresses helps us understand how you acquire and maintain your subscriber list.
+
+How do you currently manage and remove email addresses that have unsubscribed or resulted in 
+bounce backs from your mailing list?
+Please explain if you have an automated process in place that handles unsubscribes when recipients 
+click on the 'unsubscribe' link in your emails. Additionally, if you receive bounce/undeliverable 
+notifications, can you include how you handle those and whether you have any mechanism to 
+automatically remove email addresses that result in consistent bounces.
